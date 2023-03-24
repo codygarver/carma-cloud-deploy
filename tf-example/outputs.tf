2456b65d0cab6b532c8b624c07ea1d6b93667488
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2023-03-24 15:45:37 -0400

    Update outputs.tf

--- tf-example/outputs.tf
+++ tf-example/outputs.tf
@@ -1,8 +1,8 @@
-output ""public_dns" {
+output "public_dns" {
   description = "the public DNS name assigned to the instance. "
   value       = try (aws_instance.carmacloud-test.public_dns, "")
 }
-output ""public_ip" {
+output "public_ip" {
   description = "the public DNS name assigned to the instance. "
   value       = try (aws_instance.carmacloud-test.public_ip, "")
 }
